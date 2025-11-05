f3a3f8a07480f9958c674d9c485c06bc606c09b0
Chris Mlinac <cmlinac@gmail.com>
2025-11-05 16:47:26 -0600

    serious cleanup of unused files, 404 page tweaks as a bonus

--- src/config/footer.js
+++ src/config/footer.js
@@ -4,41 +4,14 @@ export const footerSocials = [
     url: 'https://twitter.com/ichbindev',
     icon: "mdi:twitter",
   },
-  // {
-  //   name: 'Youtube',
-  //   url: 'https://youtube.com/@jaydanurwin',
-  //   icon: "mdi:youtube",
-  // },
   {
     name: 'Github',
     url: 'https://github.com/ichbindev',
     icon: "mdi:github",
   },
-  // {
-  //   name: 'Discord',
-  //   url: 'https://astro.build/chat',
-  //   icon: "ic:baseline-discord",
-  // },
 ]
 
 export const footerLists = [
-	// {
-	// 	title: 'Landing Pages',
-	// 	items: [
-	// 		{
-	// 			title: 'Landing Page 1',
-	// 			slug: '/landing-pages/landing-1',
-	// 		},
-	// 		{
-	// 			title: 'Landing Page 2',
-	// 			slug: '/landing-pages/landing-2',
-	// 		},
-  //     // {
-	// 		// 	title: 'Landing Page 3',
-	// 		// 	slug: '/landing-pages/landing-3',
-	// 		// },
-	// 	],
-	// },
 	{
 		title: 'Company',
 		items: [
@@ -46,35 +19,10 @@ export const footerLists = [
 				title: 'About',
 				slug: '/company/about',
 			},
-			// {
-			// 	title: 'Blog',
-			// 	slug: '/blog',
-			// },
 			{
 				title: 'Contact',
 				slug: '/company/contact',
 			},
 		],
 	},
-	// {
-	// 	title: 'Theme',
-	// 	items: [
-	// 		{
-	// 			title: 'Get Started',
-	// 			slug: '/theme/get-started',
-	// 		},
-	// 		{
-	// 			title: 'Style Guide',
-	// 			slug: '/theme/style-guide',
-	// 		},
-  //     {
-  //       title: 'Theme Setup',
-  //       slug: '/theme/theme-setup',
-  //     },
-  //     {
-  //       title: 'Customizing Odyssey',
-  //       slug: '/theme/customizing-odyssey',
-  //     }
-	// 	],
-	// },
 ];
